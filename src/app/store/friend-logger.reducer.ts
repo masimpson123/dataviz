9983bc77e171402589b6fd0bfcdafe88c86bfc49
Living Room <livingroom@Living-Room-Mac-Mini.local>
2021-06-01 16:53:20 -0500

    code clean up

--- src/app/store/friend-logger.reducer.ts
+++ src/app/store/friend-logger.reducer.ts
@@ -7,7 +7,7 @@ export let initialState:Map<string,Person> = new Map();
 const _friendLoggerReducer = createReducer(
   initialState,
   on(addPerson, (state,action) => initialState.set(action.person.name,action.person)),
-  on(reset, (state,action) => initialState = new Map<string,Person>()),);
+  on(reset, (state,action) => initialState = new Map<string,Person>()));
 
 export function friendLoggerReducer(state:Map<string,Person>|undefined, action: Action) {
   return _friendLoggerReducer(state, action);
