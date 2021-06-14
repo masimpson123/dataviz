ce133feb6a46f73dcedf9d9f9e79536e099723be
Living Room <livingroom@Living-Room-Mac-Mini.local>
2021-06-14 13:40:16 -0500

    environment config update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
   production: true
 };
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyD2EwhvejuO9cV-TFtSEoDI80new1FCtdk",
+  authDomain: "michael-simpson.firebaseapp.com",
+  projectId: "michael-simpson",
+  storageBucket: "michael-simpson.appspot.com",
+  messagingSenderId: "79005574161",
+  appId: "1:79005574161:web:cce94ba9232e434db3fdb0",
+  measurementId: "G-1R4BG2ZE65"
+};
