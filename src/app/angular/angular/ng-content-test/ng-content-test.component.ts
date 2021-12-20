c7fb0d4f01dca48fe53b0a3099a3c7c06ddde5bf
Living Room <livingroom@Living-Room-Mac-Mini.local>
2021-12-20 13:46:25 -0600

    ng-content-test

---
+++ src/app/angular/angular/ng-content-test/ng-content-test.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ng-content-test',
+  templateUrl: './ng-content-test.component.html',
+  styleUrls: ['./ng-content-test.component.css']
+})
+export class NgContentTestComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
