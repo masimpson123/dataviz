d080c9c8bfd22eddef3b517e0a56d915c9cf0ef5
Living Room <livingroom@Living-Room-Mac-Mini.local>
2023-04-08 20:32:49 -0500

    michael-io-app

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/friend-logger'),
+      dir: require('path').join(__dirname, './coverage/michael-io-app'),
       subdir: '.',
       reporters: [
         { type: 'html' },
